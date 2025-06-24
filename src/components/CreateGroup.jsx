ca85f4f300fc31315211c930eb8ec174e65c91e4
rudrasgithub <rudramanaidu99@gmail.com>
2025-06-24 12:20:45 +0000

    init

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -39,14 +39,14 @@ const CreateGroup = ({ setOpen }) => {
     };
 
     return (
-        <div className='relative bg-white pt-6 pl-8 pr-8 pb-4 w-[550px] max-sm:w-[350px] flex flex-col space-y-5 rounded-lg'>
+        <div className='relative bg-white pt-6 pl-8 max-sm:pl-4 pr-8 max-sm:pr-3 pb-4 w-[550px] max-sm:w-[350px] flex flex-col space-y-5 rounded-lg'>
             <h1 className="font-semibold text-2xl">Create New Group</h1>
             <form onSubmit={handleCreateGroup} className="space-y-8">
-                <div className="flex justify-center items-center space-x-10">
+                <div className="flex justify-center items-center space-x-10 max-sm:space-x-4">
                     <span className="text-nowrap text-xl font-semibold">Group Name</span>
                     <input
                         type="text"
-                        className="border border-gray-300 rounded-3xl px-6 py-2 w-full"
+                        className="border border-gray-300 rounded-3xl px-6 max-sm:px-4 py-2 w-full"
                         placeholder="Enter group name"
                         value={groupName}
                         onChange={(e) => setGroupName(e.target.value)}
@@ -54,7 +54,7 @@ const CreateGroup = ({ setOpen }) => {
                     />
                 </div>
 
-                <div className="flex space-x-14">
+                <div className="flex space-x-14 max-sm:space-x-7">
                     <span className="text-xl font-semibold">Choose Color</span>
                     <div className="flex gap-2">
                         {colors.map((color, index) => (
@@ -71,10 +71,10 @@ const CreateGroup = ({ setOpen }) => {
                     </div>
                 </div>
 
-                <div className="flex justify-end">
+                <div className="flex justify-end max-sm:justify-center">
                     <button
                         type="submit"
-                        className={`bg-[#001F8B] w-fit text-white px-10 py-1 rounded-lg 
+                        className={`bg-[#001F8B] w-fit text-white text-xl px-10 max-sm:px-20 py-1 max-sm:py-2 rounded-lg 
                             ${!groupName.trim() ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
                         disabled={!groupName.trim()}
                     >
